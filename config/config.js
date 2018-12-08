d35cf1af73b8ef1cde7ae851e827d458c03598e3
nurba <nurba47@gmail.com>
2018-12-08 15:04:11 +0600

    up

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  github: {
+      clientID: '33fafc27f2b47ed960a5',
+      clientSecret: '2225b1e1dc4045ba9ff8bb5e7d6790415995fa23',
+      callbackURL: "http://localhost:3000/auth/github/callback"
+  }
+};
